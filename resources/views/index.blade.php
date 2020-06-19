687718617ea700f8841e3a674da6f16200357621
Tido <99051567@mydavinci.nl>
2020-06-19 13:12:06 +0200

    Slight update to index.blade.php
    commit due to recent clash which is now resolved in gitkraken

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<div class='col-12 h-100 mx-auto bg-dark'>
+
+<!--Main overview of chats for user -->
+<div class='col-4'>
+<?php //foreach ($chatvar as $row){ ?>
+    <div class="card">
+    <img class="card-img-top" src="" alt="">
+    <div class="card-body">
+        <h4 class="card-title"> <?= $chatname ?> </h4>
+        <p class="card-text"><?= $lastmsg ?></p>
+    </div>
+</div>
+<?php // } ?>
+</div>
+
+</div>
+>>>>>>> Stashed changes
